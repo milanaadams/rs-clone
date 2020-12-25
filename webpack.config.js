db8a4d3db80e526c389fa07e4a494b207c41a867
Natallia <natallia@192.168.100.5>
2020-12-26 01:29:41 +0300

    setup: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: 'source-map',
+    watch: !isProduction,
+    entry: ['./src/index.js', './src/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      publicPath: './',
+      filename: 'script.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/i,
+          use: [
+              {
+                  loader: 'file-loader',
+                  options: {
+                      name: '[name].[ext]',
+                      outputPath: 'assets/',
+                    }
+              }
+          ]
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader'
+        }
+      ]
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(), 
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css'
+    }),
+  ]
+  }
+
+  return config;
+}
