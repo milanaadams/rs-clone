cb7bd3308afad8854bb3f1358dd5f2707480652e
Natallia Adams <milanaadams@gmail.com>
2020-12-28 17:24:33 +0300

    feat: add create class for dom elemts, add abstract class for custom events

---
+++ src/modules/abstract/abstract.js
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+export default class abstract {
+  constructor() {
+    this.evtArr = [];
+
+    this.evtArr.forEach((evtName) => {
+      document.addEventListener(evtName, (evt) => this.catchEvent(evtName, evt.detail));
+    });
+  }
+
+  createCustomEvent(eventName, eventObj, timeOut) {
+    const customEvt = new CustomEvent(eventName, {
+      detail: eventObj,
+    });
+
+    if (timeOut) {
+      setTimeout(() => document.dispatchEvent(customEvt), timeOut);
+    } else {
+      document.dispatchEvent(customEvt);
+    }
+  }
+
+  catchEvent(eventName) {
+    if (this.evtArr.indexOf(eventName) > -1) {
+      throw new Error('Not implemented exception.');
+    }
+  }
+}
