1e0a79d3f802b968b39a62dcf36e8a71b19e2876
f19m <leomelik@gmail.com>
2021-01-31 14:53:01 +0500

    fix: fix popup TS

--- src/modules/popup/popup.ts
+++ src/modules/popup/popup.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/lines-between-class-members */
 import create from '../utils/create';
 import Abstract from '../abstract/abstract';
 
+// ts
+import { Dictionary } from '../../types/typings';
+
 export default class Popup extends Abstract {
-  constructor(parent, content) {
+  parent: HTMLElement;
+  content: any;
+  elements: Dictionary<HTMLElement>;
+
+  constructor(parent: HTMLElement, content: any) {
     super();
     this.parent = parent;
     this.content = content;
@@ -10,7 +18,7 @@ export default class Popup extends Abstract {
     this.loadPopup();
   }
 
-  loadPopup() {
+  loadPopup(): void {
     this.elements.blackOut = create('div', 'blackout');
     this.parent.prepend(this.elements.blackOut);
 
@@ -22,14 +30,14 @@ export default class Popup extends Abstract {
     this.parent.prepend(this.elements.popup);
   }
 
-  closePopup() {
+  closePopup(): void {
     document.body.classList.remove('lock');
     this.elements.popup.remove();
     this.elements.blackOut.remove();
     this.createCustomEvent('removeIconBoard');
   }
 
-  catchEvent(eventName) {
+  catchEvent(eventName: string): void {
     if (this.evtArr.indexOf(eventName) === -1) {
       throw new Error('Wrong custom event name.');
     }
